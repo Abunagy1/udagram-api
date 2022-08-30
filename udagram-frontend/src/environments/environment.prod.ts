daf26e0e6e660e574f5cf2b8bb8d4b876603c966
Abunagy <44762983+Abunagy1@users.noreply.github.com>
2022-08-30 19:56:28 +0200

    update Env link
    change comment form and cotes to single instead of double

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  //apiHost: "http://localhost:8080/api/v0",
-  apiHost: "http://Udagramapi-env-1.eba-mfqtzzkv.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  // apiHost: "http://localhost:8080/api/v0",
+  apiHost: 'http://Udagram-env.eba-ehcstbcn.us-east-1.elasticbeanstalk.com/api/v0',
+  
 };
 
 /*
